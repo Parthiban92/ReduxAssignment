36fb6a21651f0c8919c7290caf9fde4137f0bd2e
Parthiban <parthiban.n@navoinc.com>
2020-08-19 10:18:32 +0530

    Updates

---
+++ precache-manifest.5da0b48d02e720de75e2e750521cb06b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db5210bedb087e2bd2c7680e9c80263d",
+    "url": "/ReduxAssignment/index.html"
+  },
+  {
+    "revision": "74638c3ee81f6e0c32fb",
+    "url": "/ReduxAssignment/static/css/main.ac1dad87.chunk.css"
+  },
+  {
+    "revision": "1cc7eeeba16b6eae273c",
+    "url": "/ReduxAssignment/static/js/2.1de5df0e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReduxAssignment/static/js/2.1de5df0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74638c3ee81f6e0c32fb",
+    "url": "/ReduxAssignment/static/js/main.c0fc5311.chunk.js"
+  },
+  {
+    "revision": "05b6f59212ef4e0f41bf",
+    "url": "/ReduxAssignment/static/js/runtime-main.c401fd15.js"
+  }
+]);
